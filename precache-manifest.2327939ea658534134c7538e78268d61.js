720fed1afce04c33a4f6d69a2e7383d0ef44cf5e
ananthu <ananthu4uuu@gmail.com>
2023-03-08 02:08:18 +0530

    e-commerce site test1

---
+++ precache-manifest.2327939ea658534134c7538e78268d61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "610086edbe9fa7be2873",
+    "url": "/static/css/main.d7c5a774.chunk.css"
+  },
+  {
+    "revision": "610086edbe9fa7be2873",
+    "url": "/static/js/main.610086ed.chunk.js"
+  },
+  {
+    "revision": "df93f121281cbbc60dcc",
+    "url": "/static/css/1.8f7cc955.chunk.css"
+  },
+  {
+    "revision": "df93f121281cbbc60dcc",
+    "url": "/static/js/1.df93f121.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "54eb75deeaf81892244fb7c378737faf",
+    "url": "/index.html"
+  }
+];
